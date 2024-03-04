217629ceebcdfb08b88cb19185595d592e8efdbc
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2024-03-04 08:47:02 -0300

    finalização da base de dados

--- Cozastore/Models/Produto.cs
+++ Cozastore/Models/Produto.cs
@@ -54,4 +54,6 @@ public class Produto
     public int CategoriaId { get; set; }
     [ForeignKey("CategoriaId")]
     public Categoria Categoria { get; set; } 
+
+    public ICollection<Estoque> Estoque { get; set; }
 }
