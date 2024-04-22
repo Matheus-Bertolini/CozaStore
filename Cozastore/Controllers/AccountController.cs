11afc6e53d3bc21371289b783196546989af82b5
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2024-04-22 08:44:17 -0300

    criação do login e da autenticação

--- Cozastore/Controllers/AccountController.cs
+++ Cozastore/Controllers/AccountController.cs
@@ -1,5 +1,5 @@
 using System.Net.Mail;
-using CozaStore.ViewModels;
+using Cozastore.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -41,8 +41,29 @@ public class AccountController : Controller
             string userName = login.Email;
             if (IsValidEmail(userName))
             {
+                var user = await _userManager.FindByEmailAsync(userName);
+                if(user != null)
+                    userName = user.UserName;
                 
             }
+
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Senha, login.Lembrar, lockoutOnFailure: true
+            );
+
+            if(result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário {login.Email} acessou o sistema!");
+                return LocalRedirect(login.UrlRetorno);
+            }
+
+            if(result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usuário {login.Email} está bloqueado!");
+                ModelState.AddModelError(string.Empty, "Conta bloqueada! Aguarde alguns minutos para continuar!");
+            }
+
+            ModelState.AddModelError(string.Empty, "Usuário e/ou Senha Inválidos!!!");
         }
         return View(login);
     }
