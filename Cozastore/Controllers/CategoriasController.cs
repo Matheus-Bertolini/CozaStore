79857473f61fc9fcfa4b22e56212335fa00ea83b
Matheus-Bertolini <nunesmath890@gmail.com>
2024-05-20 08:40:36 -0300

    realizado o metodo de login para usuario e admin e adicionado o template

--- Cozastore/Controllers/CategoriasController.cs
+++ Cozastore/Controllers/CategoriasController.cs
@@ -3,9 +3,12 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Cozastore.Data;
 using Cozastore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Cozastore.Controllers;
 
+[Authorize(Roles = "Administrador, Funcionário")]
 public class CategoriasController : Controller
 {
     private readonly AppDbContext _context;
@@ -59,22 +62,28 @@ public class CategoriasController : Controller
     {
         if (ModelState.IsValid)
         {
-            _context.Add(categoria);
-            await _context.SaveChangesAsync();
-            if (Foto != null)
+            if (!CategoriaExists(categoria))
             {
-                string fileName = categoria.Id + Path.GetExtension(Foto.FileName);
-                string upload = Path.Combine(_host.WebRootPath, "img\\categorias");
-                string newFile = Path.Combine(upload, fileName);
-                using (var stream = new FileStream(newFile, FileMode.Create))
-                {
-                    Foto.CopyTo(stream);
-                }
-                categoria.Foto = "\\img\\categorias\\" + fileName;
+                    
+                _context.Add(categoria);
                 await _context.SaveChangesAsync();
-            }
-
-            return RedirectToAction(nameof(Index));
+                if (Foto != null)
+                {
+                    string fileName = categoria.Id + Path.GetExtension(Foto.FileName);
+                    string upload = Path.Combine(_host.WebRootPath, "img\\categorias");
+                    string newFile = Path.Combine(upload, fileName);
+                    using (var stream = new FileStream(newFile, FileMode.Create))
+                    {
+                        Foto.CopyTo(stream);
+                    }
+                    categoria.Foto = "\\img\\categorias\\" + fileName;
+                    await _context.SaveChangesAsync();
+                }
+                TempData["Sucess"] = $"A categoria '{categoria.Nome}' foi adicionada com sucesso";
+                return RedirectToAction(nameof(Index));
+            } 
+            else
+                ModelState.AddModelError(string.Empty, "Nome já cadastrado!");
         }
         ViewData["CategoriaPaiId"] = new SelectList(_context.Categorias, "Id", "Nome", categoria.CategoriaPaiId);
         return View(categoria);
@@ -118,7 +127,7 @@ public class CategoriasController : Controller
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!CategoriaExists(categoria.Id))
+                if (!CategoriaExists(categoria))
                 {
                     return NotFound();
                 }
@@ -167,9 +176,12 @@ public class CategoriasController : Controller
         return RedirectToAction(nameof(Index));
     }
 
-    private bool CategoriaExists(int id)
+    private bool CategoriaExists(Categoria categoria)
     {
-        return _context.Categorias.Any(e => e.Id == id);
+        if(categoria.Id == 00)
+            return _context.Categorias.Any(e => e.Nome == categoria.Nome);
+        else
+            return _context.Categorias.Any(e => e.Id == categoria.Id);
     }
 }
 
--- Cozastore/Controllers/CoresController.cs
+++ Cozastore/Controllers/CoresController.cs
@@ -7,151 +7,153 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Cozastore.Data;
 using Cozastore.Models;
+using Microsoft.AspNetCore.Authorization;
 
-namespace Cozastore.Controllers
+namespace Cozastore.Controllers;
+
+[Authorize(Roles = "Administrador, Funcionário")]
+public class CoresController : Controller
 {
-    public class CoresController : Controller
+    private readonly AppDbContext _context;
+
+    public CoresController(AppDbContext context)
     {
-        private readonly AppDbContext _context;
+        _context = context;
+    }
 
-        public CoresController(AppDbContext context)
+    // GET: Cores
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Cores.ToListAsync());
+    }
+
+    // GET: Cores/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        // GET: Cores
-        public async Task<IActionResult> Index()
+        var cor = await _context.Cores
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (cor == null)
         {
-            return View(await _context.Cores.ToListAsync());
+            return NotFound();
         }
 
-        // GET: Cores/Details/5
-        public async Task<IActionResult> Details(int? id)
+        return View(cor);
+    }
+
+    // GET: Cores/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Cores/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Nome,CodigoHexa")] Cor cor)
+    {
+        if (ModelState.IsValid)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var cor = await _context.Cores
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (cor == null)
-            {
-                return NotFound();
-            }
-
-            return View(cor);
-        }
-
-        // GET: Cores/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Cores/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,CodigoHexa")] Cor cor)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(cor);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(cor);
-        }
-
-        // GET: Cores/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var cor = await _context.Cores.FindAsync(id);
-            if (cor == null)
-            {
-                return NotFound();
-            }
-            return View(cor);
-        }
-
-        // POST: Cores/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,CodigoHexa")] Cor cor)
-        {
-            if (id != cor.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(cor);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CorExists(cor.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(cor);
-        }
-
-        // GET: Cores/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var cor = await _context.Cores
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (cor == null)
-            {
-                return NotFound();
-            }
-
-            return View(cor);
-        }
-
-        // POST: Cores/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var cor = await _context.Cores.FindAsync(id);
-            if (cor != null)
-            {
-                _context.Cores.Remove(cor);
-            }
-
+            _context.Add(cor);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        return View(cor);
+    }
 
-        private bool CorExists(int id)
+    // GET: Cores/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
         {
-            return _context.Cores.Any(e => e.Id == id);
+            return NotFound();
         }
+
+        var cor = await _context.Cores.FindAsync(id);
+        if (cor == null)
+        {
+            return NotFound();
+        }
+        return View(cor);
+    }
+
+    // POST: Cores/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,CodigoHexa")] Cor cor)
+    {
+        if (id != cor.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(cor);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CorExists(cor.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(cor);
+    }
+
+    // GET: Cores/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var cor = await _context.Cores
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (cor == null)
+        {
+            return NotFound();
+        }
+
+        return View(cor);
+    }
+
+    // POST: Cores/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var cor = await _context.Cores.FindAsync(id);
+        if (cor != null)
+        {
+            _context.Cores.Remove(cor);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool CorExists(int id)
+    {
+        return _context.Cores.Any(e => e.Id == id);
     }
 }
+
